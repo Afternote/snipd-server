60b8924852fe523752295411fab1f4e0181a0b47
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-06-13 00:03:39 +0530

    basic structure

--- main.go
+++ main.go
@@ -1,49 +1,46 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gofor-little/env"
+	"github.com/integrationninjas/go-app/handlers"
 )
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.POST("/albums", postAlbums)
+	// load .env file
+	// godotenv package
+	port := goDotEnvVariable("PORT")
+	if port == "" {
+		port = "8080" // Default port
+	}
 
-    router.Run("localhost:8080")
+	// Initialize logging
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Register API handlers
+	http.HandleFunc("/", handlers.HelloHandler)
+	http.HandleFunc("/items", handlers.ItemsHandler)
+	http.HandleFunc("/randomuser", handlers.GetRandomUser)
+
+	fmt.Println("Server listening on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := env.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
-
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-    var newAlbum album
-
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if err := c.BindJSON(&newAlbum); err != nil {
-        return
-    }
-
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
